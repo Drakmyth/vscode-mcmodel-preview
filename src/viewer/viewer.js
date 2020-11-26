3ce0bb733772044389671a347bde8cc3371133f6
Shaun Hamman <shaun.hamman@gmail.com>
2020-11-26 13:53:34 -0800

    Render model and add camera rotation controls

--- src/viewer/viewer.js
+++ src/viewer/viewer.js
@@ -1,43 +1,32 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const cameraAnchor = new THREE.Object3D();
+cameraAnchor.add(camera);
+scene.add(cameraAnchor);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-var faceIndices = ['a', 'b', 'c'];
-var vertexIndex, point;
-geometry.faces.forEach(function(face) {
-    for (var i = 0; i < 3; i++) {
-        vertexIndex = face[faceIndices[i]];
-        point = geometry.vertices[vertexIndex];
-        color = new THREE.Color(
-            Math.abs(face.normal.x),
-            Math.abs(face.normal.y),
-            Math.abs(face.normal.z)
-        );
-        face.vertexColors[i] = color;
-    }
-})
-const material = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
+const normalMap = {
+    up: new THREE.Vector3(0, 1, 0),
+    down: new THREE.Vector3(0, -1, 0),
+    east: new THREE.Vector3(1, 0, 0),
+    west: new THREE.Vector3(-1, 0, 0),
+    north: new THREE.Vector3(0, 0, 1),
+    south: new THREE.Vector3(0, 0, -1)
 }
-
-animate();
 
 var tanFOV = Math.tan(((Math.PI / 180) * camera.fov / 2));
 var windowHeight = window.innerHeight;
 
-window.addEventListener('resize', onWindowResize, false);
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+animate();
+
+window.addEventListener('resize', onWindowResize);
+window.addEventListener('message', onMessageReceived);
 
 function onWindowResize(event) {
     camera.aspect = window.innerWidth / window.innerHeight;
@@ -48,12 +37,101 @@ function onWindowResize(event) {
     renderer.render(scene, camera);
 }
 
+var dragging = false;
+var mousex = 0;
+var mousey = 0;
+var rollMode = false;
+
+window.addEventListener('mousemove', onMouseMove);
+window.addEventListener('mousedown', onMouseDown);
+window.addEventListener('mouseup', onMouseUp);
+
+function onMouseMove(event) {
+    if (!dragging) return;
+
+    if (rollMode) {
+        const deltax = mousex - event.x;
+        cameraAnchor.rotateOnAxis(new THREE.Vector3(0, 0, 1), deltax / -200)
+        mousex = event.x;
+    } else {
+        const deltax = mousex - event.x;
+        const deltay = mousey - event.y;
+        cameraAnchor.rotateOnAxis(new THREE.Vector3(0, 1, 0), deltax / 200)
+        cameraAnchor.rotateOnAxis(new THREE.Vector3(1, 0, 0), deltay / 200)
+        mousex = event.x;
+        mousey = event.y;
+    }
+}
+
+function onMouseDown(event) {
+    dragging = true;
+    mousex = event.x;
+    mousey = event.y;
+    rollMode = event.ctrlKey;
+}
+
+function onMouseUp(event) {
+    dragging = false;
+    rollMode = false;
+}
+
 function onMessageReceived(event) {
     const message = event.data;
 
-    switch(message.command) {
+    switch (message.command) {
         case 'update':
             const model = message.data;
-            console.log(JSON.stringify(model));
+            scene.clear();
+            scene.add(cameraAnchor);
+            const material = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors });
+
+            for (const element of model.elements) {
+                const scale = new THREE.Vector3(
+                    element.to[0] - element.from[0],
+                    element.to[1] - element.from[1],
+                    element.to[2] - element.from[2]);
+                const position = new THREE.Vector3(element.from[0] + scale.x / 2,
+                                                   element.from[1] + scale.y / 2,
+                                                   element.from[2] + scale.z / 2);
+
+                const geometry = new THREE.BoxGeometry(scale.x, scale.y, scale.z);
+                var facesToRemove = [];
+
+                if (element.faces) {
+
+                    var faceNormalsToKeep = []
+                    for (var dir in element.faces) {
+                        faceNormalsToKeep.push(normalMap[dir]);
+                    }
+
+                    for (var face of geometry.faces) {
+                        if (!faceNormalsToKeep.find((normal) => normal.x === face.normal.x &&
+                                                                normal.y === face.normal.y &&
+                                                                normal.z === face.normal.z)) {
+                            facesToRemove.push(face);
+                        }
+                    }
+                }
+
+                for(var face of facesToRemove) {
+                    const faceIndex = geometry.faces.indexOf(face);
+                    geometry.faces.splice(faceIndex, 1);
+                }
+
+                geometry.faces.forEach(function (face) {
+                    for (var i = 0; i < 3; i++) {
+                        color = new THREE.Color(
+                            Math.abs(face.normal.x),
+                            Math.abs(face.normal.y),
+                            Math.abs(face.normal.z)
+                        );
+                        face.vertexColors[i] = color;
+                    }
+                });
+                const cube = new THREE.Mesh(geometry, material);
+                cube.position = position;
+                camera.position.z = 25;
+                scene.add(cube);
+            }
     }
 }
